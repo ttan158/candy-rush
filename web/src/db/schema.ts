d974f752e4d1e944e8668cc8a9d15a7e6391f093
Sunny Setia <sunnyysetia@gmail.com>
2025-11-01 12:33:44 +1300

    db schema done

--- web/src/db/schema.ts
+++ web/src/db/schema.ts
@@ -1,10 +1,51 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  doublePrecision,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  email: text("email").notNull().unique(),
-  password: text("password").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+export const calls = pgTable("calls", {
+  id: uuid().primaryKey().defaultRandom(),
+  callerNumber: text().notNull(),
+  transcript: text().notNull(),
+  recordingUrl: text().notNull(),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+});
+
+export const candies = pgTable("candies", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull().unique(),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+});
+
+export const houses = pgTable("houses", {
+  id: uuid().primaryKey().defaultRandom(),
+  latitude: doublePrecision().notNull(),
+  longitude: doublePrecision().notNull(),
+  address: text().notNull(),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+});
+
+export const reports = pgTable("reports", {
+  id: uuid().primaryKey().defaultRandom(),
+  callId: uuid()
+    .notNull()
+    .references(() => calls.id, { onDelete: "cascade" }),
+  houseId: uuid()
+    .notNull()
+    .references(() => houses.id, { onDelete: "cascade" }),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+});
+
+export const reportCandies = pgTable("report_candies", {
+  id: uuid().primaryKey().defaultRandom(),
+  reportId: uuid()
+    .notNull()
+    .references(() => reports.id, { onDelete: "cascade" }),
+  candyId: uuid()
+    .notNull()
+    .references(() => candies.id, { onDelete: "cascade" }),
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
 });
